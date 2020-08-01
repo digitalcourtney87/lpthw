d4b983b60b8163024598bba3601677d0882792cb
Courtney Allen <43904694+digitalcourtney87@users.noreply.github.com>
2020-08-01 17:11:08 +0100

    ex1-4

---
+++ ex1.py
@@ -0,0 +1,9 @@
+# This creates a comment
+
+print("Hello World")
+print("Hello again")
+print("I like typing this.")
+print("This is fun.")
+print('Yay! Printing.')
+print("I'd much rather you 'not'.")
+print('I "said" do not touch this.')
---
+++ ex3.py
@@ -0,0 +1,33 @@
+# prints the string "I will now count my chickens:"
+print("I will now count my chickens:")
+
+# prints hens and roosters and the calculation after each string
+print("Hens", 25 + 30 / 6)
+print("Roosters", 100 - 25 * 3 % 4)
+
+# prints the string "I wil now count my eggs"
+print("I will now count my eggs:")
+
+# prints the calculation
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+
+# prints the string "Is it true that....?"
+print("Is it true that 3 + 2 < 5 - 7?")
+
+# Asks whether 3+2 is less that 5-7
+print(3 + 2 < 5 - 7)
+
+# prints the string asking the question then does the calculation after the comma
+print("What is 3 + 2?", 3 + 2)
+print("What is 5 - 7?", 5 - 7)
+
+# prints a statement
+print("Oh, that's why it's false.")
+
+# prints a statement
+print("How about some more.")
+
+# prints some more strings and calculations with greater, greater or equal and less or equal
+print("Is it greater?", 5 > -2)
+print("is it greater or equal?", 5>= -2)
+print("Is it less or equal?", 5 <= -2)
