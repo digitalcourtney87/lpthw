df18277e328c75188a817dc200493630d9219ae8
Courtney Allen <43904694+digitalcourtney87@users.noreply.github.com>
2020-08-02 21:41:29 +0100

    Changes to ex5.py

--- ex4.py
+++ ex4.py
@@ -5,6 +5,7 @@ passengers = 90
 cars_not_driven = cars - drivers
 cars_driven = drivers
 carpool_capacity = cars_driven * space_in_a_car
+# Used // floor division to create a whole number instead of the previous float
 average_passengers_per_car = passengers // cars_driven
 
 print("There are", cars, "cars available.")
